bd14c4a93f0e98b13b2bbc30174447b84cfc805d
tristanbuls <131413775+tristanbuls@users.noreply.github.com>
2023-11-28 16:37:52 +0100

    Add files via upload

---
+++ Notendurchschnitt/noten/src/main/java/edu/fra/uas/noten/NotenApplication.java
@@ -0,0 +1,13 @@
+package edu.fra.uas.noten;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotenApplication.class, args);
+	}
+
+}
---
+++ Notendurchschnitt/noten/src/test/java/edu/fra/uas/noten/NotenApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.fra.uas.noten;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
